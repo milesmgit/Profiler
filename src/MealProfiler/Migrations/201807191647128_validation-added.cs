a4ac339583523fba88c2ad309f6e7d48be969c7d
Code Louisville 1 <milesm427@gmail.com>
2018-07-19 12:51:07 -0400

    Added a migration and updated database

---
+++ src/MealProfiler/Migrations/201807191647128_validation-added.cs
@@ -0,0 +1,16 @@
+namespace MealProfiler.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class validationadded : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
